cf678bd4e4cff6151902e0263ecea153703176d4
SumitUjjwal <sumitonboard@gmail.com>
2023-07-18 20:56:41 +0530

    created controller for user authentication

---
+++ backend/controllers/userAuth.py
@@ -0,0 +1,107 @@
+from flask import Flask, jsonify, request
+from models.model import User
+from pymongo.errors import DuplicateKeyError
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+import bcrypt
+import jwt
+import datetime
+from dotenv import load_dotenv
+import os
+
+# Environment variables
+load_dotenv()
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+# Token expiration time
+JWT_EXPIRATION_DELTA = datetime.timedelta(days=1)
+
+
+# User Signup
+def signup():
+    try:
+        data = request.get_json()
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        email = data.get("email")
+        password = data.get("password")
+
+        if not first_name or not last_name or not email or not password:
+            return jsonify({"error": "All fields are required"}), 400
+
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        user_data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": hashed_password,
+        }
+
+        # Check if the email already exists
+        if User.find_one({"email": email}):
+            return jsonify({"error": "Email already registered"}), 409
+
+        # Insert the new user into the database
+        user_id = User.insert_one(user_data).inserted_id
+
+        return (
+            jsonify(
+                {
+                    "OK": True,
+                    "message": "User registered successfully",
+                    "user_id": str(user_id),
+                }
+            ),
+            201,
+        )
+    except DuplicateKeyError:
+        return jsonify({"error": "Email already registered"}), 409
+    except Exception as e:
+        return (
+            jsonify({"error": "An unexpected error occurred", "details": str(e)}),
+            500,
+        )
+
+
+# User login
+def login():
+    try:
+        data = request.get_json()
+        email = data.get("email")
+        password = data.get("password")
+
+        if not email or not password:
+            return jsonify({"error": "Email and password are required"}), 400
+
+        # Check if the user exists and the provided password matches
+        user = User.find_one({"email": email})
+        if user:
+            if bcrypt.checkpw(password.encode("utf-8"), user["password"]):
+                # Generate a JWT token
+                token_payload = {
+                    "user_id": str(user["_id"]),
+                    "exp": datetime.datetime.utcnow() + JWT_EXPIRATION_DELTA,
+                }
+                jwt_token = jwt.encode(token_payload, JWT_SECRET_KEY, algorithm="HS256")
+
+                return (
+                    jsonify(
+                        {
+                            "OK": True,
+                            "message": "Login successful",
+                            "user_id": str(user["_id"]),
+                            "token": jwt_token,
+                        }
+                    ),
+                    200,
+                )
+            else:
+                return jsonify({"error": "Invalid email or password"}), 401
+        else:
+            return jsonify({"error": "Invalid email or password"}), 401
+    except Exception as e:
+        return (
+            jsonify({"error": "An unexpected error occurred", "details": str(e)}),
+            500,
+        )
