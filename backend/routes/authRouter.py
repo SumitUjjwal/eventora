cf6a27a85b674c8173a38445bebf8410cb9b41e0
SumitUjjwal <sumitonboard@gmail.com>
2023-07-19 20:42:13 +0530

    created routes for the venue providers

--- backend/routes/authRouter.py
+++ backend/routes/authRouter.py
@@ -4,10 +4,20 @@ from controllers.userAuth import (login, signup)
 # create a new instance
 auth = Blueprint('auth', __name__)
 
+# User Authentication
 @auth.route('/user/login', methods=['POST'])
 def login_route():
     return login()
 
 @auth.route('/user/signup', methods=['POST'])
 def signup_route():
+    return signup()
+
+# Venue Provider Authentication
+@auth.route('/venue/provider/login', methods=['POST'])
+def venue_provider_login_route():
+    return login()
+
+@auth.route('/venue/provider/signup', methods=['POST'])
+def venue_provider_signup_route():
     return signup()
